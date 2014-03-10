5903ce68c4ddc0e8bdd7301ea17665f61844e6d6
PlenipotentSS <stevensonhoyt@icloud.com>
2014-03-10 04:12:14 -0700

    added stack control and page view control

--- Swing Local/SSHomeViewController.m
+++ Swing Local/SSHomeViewController.m
@@ -7,13 +7,22 @@
 //
 
 #import "SSHomeViewController.h"
+#import "SSPagedView.h"
+#import "UIColor+SwingLocal.h"
+#import "SSStackedPageView.h"
+#import "UIColor+SwingLocal.h"
 
 #define LOAD_LOGO_TRANSFORM CATransform3DMakeScale(.50, .50, .50)
 
-@interface SSHomeViewController() <UITableViewDataSource,UITableViewDelegate>
+@interface SSHomeViewController() <UITableViewDataSource,UITableViewDelegate,SSPagedViewDelegate,SSStackViewDelegate>
 
 @property (nonatomic) UIImageView *loadLogo;
 @property (weak, nonatomic) IBOutlet UITableView *theTableView;
+@property (weak, nonatomic) SSPagedView *pagedView;
+@property (nonatomic) NSMutableArray *views;
+@property (weak, nonatomic) UIPageControl *thePageControl;
+@property (nonatomic) SSStackedPageView *stackView;
+@property (nonatomic) NSMutableArray *savedCities;
 
 
 @end
@@ -72,15 +81,61 @@
     return 1;
 }
 
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == 0) {
+        return 164.f;
+    } else if (indexPath.row == 1) {
+        return 200.f;
+    } else if (indexPath.row == 2) {
+        return 200.f;
+    }
+    else return 0.f;
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     // Return the number of rows in the section.
-    return 1;
+    return 3;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"imagePeakCell";
+    NSString *CellIdentifier = @"";
+    if ( indexPath.row == 0 ){
+        CellIdentifier = @"imagePeakCell";
+    } else if (indexPath.row == 1){
+        CellIdentifier = @"savedCitiesCell";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+        if (!self.stackView) {
+            self.stackView = (SSStackedPageView*)[cell viewWithTag:1];
+            self.stackView.delegate = self;
+            self.savedCities = [[NSMutableArray alloc] init];
+            for (int i=0;i<10;i++) {
+                UIView *thisView = [[UIView alloc] initWithFrame:CGRectMake(0.f, 0.f, 200.f, 100.f)];
+                [self.savedCities addObject:thisView];
+            }
+        }
+        
+        return cell;
+    } else if (indexPath.row == 2){
+        CellIdentifier = @"favoriteOrgsCell";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+        if (!self.pagedView) {
+            self.pagedView = (SSPagedView*)[cell viewWithTag:1];
+            self.thePageControl = (UIPageControl*)[cell viewWithTag:2];
+            
+            self.pagedView.delegate = self;
+            self.pagedView.pageControl = self.thePageControl;
+            self.views = [[NSMutableArray alloc] init];
+            for (int i=0;i<10;i++) {
+                UIView *thisView = [[UIView alloc] initWithFrame:CGRectMake(0.f, 0.f, 200.f, 150.f)];
+                [self.views addObject:thisView];
+            }
+        }
+        
+        return cell;
+    }
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     UIScrollView *imageScroll = (UIScrollView*)[cell.contentView viewWithTag:1];
     imageScroll.contentSize = CGSizeMake(960.f, imageScroll.frame.size.height);
@@ -89,5 +144,65 @@
     return cell;
 }
 
+#pragma mark - IBActions Page Control
+- (IBAction)pageControlChanged:(id)sender {
+    [self.pagedView scrollToPage:[(UIPageControl*)sender currentPage]];
+}
+
+#pragma mark - paged delegate methods
+- (void)pageView:(SSPagedView *)pagedView didScrollToPageAtIndex:(NSInteger)index
+{
+    
+}
+
+- (UIView *)pageView:(SSPagedView *)pagedView entryForPageAtIndex:(NSInteger)index
+{
+    UIView *thisView = [pagedView dequeueReusableEntry];
+    if (!thisView) {
+        thisView = [self.views objectAtIndex:index];
+        thisView.backgroundColor = [UIColor burntScheme];
+        thisView.layer.cornerRadius = 5;
+        thisView.layer.masksToBounds = YES;
+    }
+    return thisView;
+}
+
+- (CGSize)sizeForPageInView:(SSPagedView *)pagedView
+{
+    return CGSizeMake(200, 150);
+}
+
+- (void)pageView:(SSPagedView *)pagedView selectedPageAtIndex:(NSInteger)index
+{
+    NSLog(@"%i",(int)index);
+}
+
+- (NSInteger)numberOfPagesInView:(SSPagedView *)pagedView
+{
+    return [self.views count];
+}
+
+#pragma mark - SSStackedView Delegate
+- (UIView *)stackView:(SSStackedPageView *)stackView pageForIndex:(NSInteger)index
+{
+    UIView *thisView = [stackView dequeueReusablePage];
+    if (!thisView) {
+        thisView = [self.savedCities objectAtIndex:index];
+        thisView.backgroundColor = [UIColor getRandomColor];
+        thisView.layer.cornerRadius = 5;
+        thisView.layer.masksToBounds = YES;
+    }
+    return thisView;
+}
+
+- (NSInteger)numberOfPagesForStackView:(SSStackedPageView *)stackView
+{
+    return [self.savedCities count];
+}
+
+- (void)stackView:(SSStackedPageView *)stackView selectedPageAtIndex:(NSInteger) index
+{
+    NSLog(@"selected page: %i",(int)index);
+}
 
 @end
